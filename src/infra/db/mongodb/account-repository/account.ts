7509b1fb7958260b587db42afe93c5cd725ee435
Everton Miranda de Oliveira <notreve.miranda@gmail.com>
2020-09-03 20:17:10 -0300

    refactor: move collection map to MongoHelper

--- src/infra/db/mongodb/account-repository/account.ts
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -8,11 +8,6 @@ export class AccountMongoRepository implements AddAccountRepository {
     const accountCollection = MongoHelper.getCollection('accounts')
     const result = await accountCollection.insertOne(accountData)
     const account = result.ops[0]
-    const { _id, ...accountWithoutId } = account
-
-    return {
-      id: _id,
-      ...accountWithoutId
-    }
+    return MongoHelper.map(account)
   }
 }
