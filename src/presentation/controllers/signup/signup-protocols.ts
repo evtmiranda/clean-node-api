cebbdaa3190d2faef705223dcf0933e9425c1439
Everton Miranda de Oliveira <notreve.miranda@gmail.com>
2020-09-03 18:58:58 -0300

    fix: export email-validator, http and controller of signup-protocols

--- src/presentation/controllers/signup/signup-protocols.ts
+++ src/presentation/controllers/signup/signup-protocols.ts
@@ -1,4 +1,6 @@
-export * from '../../protocols'
 export * from '../../protocols/email-validator'
 export * from '../../../domain/use-cases/add-account'
 export * from '../../../domain/models/account'
+export * from '../../protocols/email-validator'
+export * from '../../protocols/http'
+export * from '../../protocols/controller'
