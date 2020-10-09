8d8232ac9b1f09f9d3854c292a8c6aea58db0d45
Everton Miranda de Oliveira <notreve.miranda@gmail.com>
2020-10-09 09:00:16 -0300

    refactor: create makeSut

--- src/infra/db/mongodb/log-repository/log.spec.ts
+++ src/infra/db/mongodb/log-repository/log.spec.ts
@@ -2,6 +2,10 @@ import { Collection } from 'mongodb'
 import { MongoHelper } from '../helpers/mongo-helper'
 import { LogMongoRepository } from './log'
 
+const makeSut = (): LogMongoRepository => {
+  return new LogMongoRepository()
+}
+
 describe('LogRepository', () => {
   let logCollection: Collection
 
@@ -19,7 +23,7 @@ describe('LogRepository', () => {
   })
 
   test('Should create an error log', async () => {
-    const sut = new LogMongoRepository()
+    const sut = makeSut()
     await sut.logError('any_error')
     const count = await logCollection.countDocuments()
     expect(count).toBe(1)
